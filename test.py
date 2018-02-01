e8a5f4b499a57b2b5d5f995fe901fea395e2aed1
goods <1696228062@qq.com>
2018-02-01 14:53:23 +0800

    笑嘻嘻笑嘻嘻

---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/1 14:46
+# @Author  : Future
+# @Email   : XXXXXXX@163.com
+# @File    : test.py
+# @Software: PyCharm Community Edition
+print '44444444444455555555'
